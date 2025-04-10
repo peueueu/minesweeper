293e237005ee83a9afacc90a54028c04a7f35274
peueueu <silvasantospedro91@gmail.com>
2025-04-10 17:12:41 -0300

    first commit - minesweeper project

---
+++ src/main/java/org/peueueu/ms/Main.java
@@ -0,0 +1,7 @@
+package org.peueueu.ms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/org/peueueu/ms/PlaceholderTest.java
@@ -0,0 +1,14 @@
+package org.peueueu.ms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlaceholderTest {
+    @Test
+    void ifEqualsToTwo() {
+        int a = 1 + 1;
+        assertEquals(2, a);
+    }
+
+}
